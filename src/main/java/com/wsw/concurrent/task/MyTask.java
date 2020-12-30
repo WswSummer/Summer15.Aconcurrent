5ce21efc7bb976024a97e6960f7430c861a42639
wangsongwen <2544894086wsw@gmail.com>
2020-12-30 15:05:24 +0800

    2020-12-30 ThreadPool Learning

--- src/main/java/com/wsw/concurrent/task/MyTask.java
+++ src/main/java/com/wsw/concurrent/task/MyTask.java
@@ -24,12 +24,12 @@ public class MyTask implements Task {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit,
                 workQueue, threadFactory, handler);
 
-        //executor.prestartAllCoreThreads();  // 预启动所有核心线程
+        executor.prestartAllCoreThreads();  // 预启动所有核心线程
 
         for (int i = 1; i <= 10; i++) {
-            final String threadId = String.valueOf(i);
+            final String taskId = String.valueOf(i);
             executor.execute(() -> {
-                System.out.println("Thread-" + threadId + " is running..." + new Date());
+                System.out.println("Task-" + taskId + " is running... ThreadName: " + Thread.currentThread().getName() + " " + new Date());
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
@@ -37,6 +37,8 @@ public class MyTask implements Task {
                 }
             });
         }
+
+        executor.shutdown();
     }
 
     private static class MyThreadFactory implements ThreadFactory {
@@ -63,7 +65,17 @@ public class MyTask implements Task {
     }
 
     public static void main(String[] args) {
+        // 当前设备的CPU个数
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
         MyTask task = new MyTask();
         task.execute();
     }
+    /*
+        1，由于线程预启动，首先创建了1，2号线程，然后task1，task2被执行
+        2，但任务提交没有结束，此时任务task3，task6到达发现核心线程已经满了，进入等待队列；
+        3，等待队列满后创建任务线程3，4执行任务task3，task6，同时task4，task5进入队列；
+        4，此时创建线程数（4）等于最大线程数，且队列已满，所以7，8，9，10任务被拒绝；
+        5，任务执行完毕后回头来执行task4，task5，队列清空。
+     */
 }
